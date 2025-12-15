746f7dda82267ab61860d9f0b87a384a321d8d8a
sakshi sonwani <165565646+Sakshi-Sonwani@users.noreply.github.com>
2025-12-15 12:51:11 +0530

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/sakshi-sonwani-portfolio-57/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
